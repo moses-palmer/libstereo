bf3dd4b69911290c6e922261fdea59018873ade3
Moses Palmér <mosespalmer@gmail.com>
2010-08-18 16:03:43 +0000

    * stereo.c: Removed blend * private.h: Added blend2 and blend4 as utility functions.

--- private.h
+++ private.h
@@ -1,6 +1,8 @@
 #ifndef STEREO_PRIVATE_H
 #define STEREO_PRIVATE_H
 
+#include "pattern.h"
+
 #if defined __x86_64__
 # define INT_BITS 64
 #else
@@ -81,6 +83,80 @@ static inline int
 ifrac(int c)
 {
     return c ^ LIM;
+}
+
+/*
+ * Sets pixel to the linearly interpolated value calculated from the row.
+ *
+ * x is a fixed floating point value. The fractional part is used to
+ * interpolate.
+ *
+ * width is the width of the row.
+ */
+static inline void
+blend2(PatternPixel *pixel, PatternPixel *row, int x, unsigned int width)
+{
+    int x1 = unmkfix(x) % width;
+    int x2 = x1 + 1;
+    int a1, a2;
+    PatternPixel *p1, *p2;
+
+    /* Is x1 the last row? */
+    if (x2 == width) {
+        x2 = 0;
+    }
+
+#ifdef MODULUS_SIGNED
+    /* If modulus is signed, we need to correct for that */
+    if (x1 < 0) {
+        x1 += width;
+    }
+    if (x2 < 0) {
+        x2 += width;
+    }
+#endif
+
+    /* a1 is the weight of the left pixel, and a2 the weight of the right one */
+    a2 = getfrac(x);
+    a1 = ifrac(a2);
+    p1 = &row[x1];
+    p2 = &row[x2];
+
+    /* pixel = a1 * p1 + a2 * p2 */
+    pixel->r = unmkfix(mul(mkfix(p1->r), a1) + mul(mkfix(p2->r), a2));
+    pixel->g = unmkfix(mul(mkfix(p1->g), a1) + mul(mkfix(p2->g), a2));
+    pixel->b = unmkfix(mul(mkfix(p1->b), a1) + mul(mkfix(p2->b), a2));
+    pixel->a = unmkfix(mul(mkfix(p1->a), a1) + mul(mkfix(p2->a), a2));
+}
+
+/*
+ * Sets pixel to the linearly interpolated value calculated from the rows.
+ *
+ * x and y are fixed floating point values. The fractional part is used to
+ * interpolate.
+ *
+ * width is the width of the rows.
+ */
+static inline void
+blend4(PatternPixel *pixel, PatternPixel *row1, PatternPixel *row2, int x,
+    int y, unsigned int width)
+{
+    int a1, a2;
+    PatternPixel p1, p2;
+
+    blend2(&p1, row1, x, width);
+    blend2(&p2, row2, x, width);
+
+    /* a1 is the weight of the top pixel, and a2 the weight of the bottom
+       one */
+    a2 = getfrac(y);
+    a1 = ifrac(a2);
+
+    /* pixel = a1 * p1 + a2 * p2 */
+    pixel->r = unmkfix(mul(mkfix(p1.r), a1) + mul(mkfix(p2.r), a2));
+    pixel->g = unmkfix(mul(mkfix(p1.g), a1) + mul(mkfix(p2.g), a2));
+    pixel->b = unmkfix(mul(mkfix(p1.b), a1) + mul(mkfix(p2.b), a2));
+    pixel->a = unmkfix(mul(mkfix(p1.a), a1) + mul(mkfix(p2.a), a2));
 }
 
 #endif
--- stereo.c
+++ stereo.c
@@ -48,50 +48,6 @@ typedef struct {
     unsigned int channel;
 } StereoImageApplyData;
 
-/*
- * Sets pixel to the linearly interpolated value calculated from the row.
- *
- * x is a fixed floating point value. The fractional part is used to
- * interpolate.
- *
- * width is the width of the row.
- */
-static inline void
-blend(PatternPixel *pixel, PatternPixel *row, int x, unsigned int width)
-{
-    int x1 = unmkfix(x) % width;
-    int x2 = x1 + 1;
-    int a1, a2;
-    PatternPixel *p1, *p2;
-
-    /* Is x1 the last row? */
-    if (x2 == width) {
-        x2 = 0;
-    }
-
-#ifdef MODULUS_SIGNED
-    /* If modulus is signed, we need to correct for that */
-    if (x1 < 0) {
-        x1 += width;
-    }
-    if (x2 < 0) {
-        x2 += width;
-    }
-#endif
-
-    /* a1 is the weight of the left pixel, and a2 the weight of the right one */
-    a2 = getfrac(x);
-    a1 = ifrac(a2);
-    p1 = &row[x1];
-    p2 = &row[x2];
-
-    /* pixel = a1 * p1 + a2 * p2 */
-    pixel->r = unmkfix(mul(mkfix(p1->r), a1) + mul(mkfix(p2->r), a2));
-    pixel->g = unmkfix(mul(mkfix(p1->g), a1) + mul(mkfix(p2->g), a2));
-    pixel->b = unmkfix(mul(mkfix(p1->b), a1) + mul(mkfix(p2->b), a2));
-    pixel->a = unmkfix(mul(mkfix(p1->a), a1) + mul(mkfix(p2->a), a2));
-}
-
 static int
 stereo_image_apply_lines_do(StereoImageApplyData *data, int start, int end,
     int gstart, int gend)
@@ -124,7 +80,7 @@ stereo_image_apply_lines_do(StereoImageApplyData *data, int start, int end,
                 width = image->image->width;
             }
 
-            blend(d, row, sourcex, width);
+            blend2(d, row, sourcex, width);
             z += buffer->channels;
             d++;
         }
