21d7a6d04dd4d25da656892ad281403933f3dc69
Moses Palmér <mosespalmer@gmail.com>
2010-08-18 15:12:49 +0000

    * stereo.c: Corrected offset calculation for pattern pixels.

--- stereo.c
+++ stereo.c
@@ -11,7 +11,7 @@ stereo_image_create(unsigned int width, unsigned int height,
     StereoPattern *pattern, double depth)
 {
     StereoImage *result;
-    int i;
+    int frac, i;
 
     if (!pattern) {
         return NULL;
@@ -21,10 +21,13 @@ stereo_image_create(unsigned int width, unsigned int height,
     result->image = stereo_pattern_create(width, height);
     result->pattern = pattern;
 
-    /* Create the table of offsets */
+    /* Create the table of offsets, and make sure that the first offset does not
+       contain a fractional part */
+    frac = (int)(depth * mkfix(
+            sizeof(result->offsets) / sizeof(result->offsets[0])));
     for (i = 0; i < sizeof(result->offsets) / sizeof(result->offsets[0]); i++) {
         result->offsets[i] = (int)(depth * mkfix(
-            (sizeof(result->offsets) / sizeof(result->offsets[0])) - i));
+            (sizeof(result->offsets) / sizeof(result->offsets[0])) - i)) - frac;
     }
 
     return result;
@@ -57,11 +60,17 @@ static inline void
 blend(PatternPixel *pixel, PatternPixel *row, int x, unsigned int width)
 {
     int x1 = unmkfix(x) % width;
-    int x2 = (x1 + 1) % width;
+    int x2 = x1 + 1;
     int a1, a2;
     PatternPixel *p1, *p2;
 
+    /* Is x1 the last row? */
+    if (x2 == width) {
+        x2 = 0;
+    }
+
 #ifdef MODULUS_SIGNED
+    /* If modulus is signed, we need to correct for that */
     if (x1 < 0) {
         x1 += width;
     }
@@ -70,11 +79,13 @@ blend(PatternPixel *pixel, PatternPixel *row, int x, unsigned int width)
     }
 #endif
 
+    /* a1 is the weight of the left pixel, and a2 the weight of the right one */
     a2 = getfrac(x);
     a1 = ifrac(a2);
     p1 = &row[x1];
     p2 = &row[x2];
 
+    /* pixel = a1 * p1 + a2 * p2 */
     pixel->r = unmkfix(mul(mkfix(p1->r), a1) + mul(mkfix(p2->r), a2));
     pixel->g = unmkfix(mul(mkfix(p1->g), a1) + mul(mkfix(p2->g), a2));
     pixel->b = unmkfix(mul(mkfix(p1->b), a1) + mul(mkfix(p2->b), a2));
@@ -89,6 +100,7 @@ stereo_image_apply_lines_do(StereoImageApplyData *data, int start, int end,
     StereoImage *image = data->image;
     ZBuffer *buffer = data->buffer;
     PatternPixel *d = stereo_pattern_row_get(image->image, start);
+    int pattern_offset = mkfix(image->pattern->width);
 
     for (y = start; y < end; y++) {
         unsigned char *z = stereo_zbuffer_row_get(buffer, y) + data->channel;
@@ -98,11 +110,12 @@ stereo_image_apply_lines_do(StereoImageApplyData *data, int start, int end,
 
         for (x = 0; x < image->image->width; x++) {
             int offset = data->image->offsets[*z];
-            int sourcex = mkfix(x - image->pattern->width) - offset;
+            int sourcex = mkfix(x) - pattern_offset - offset;
             PatternPixel *row;
             unsigned int width;
 
-            if (!offset || sourcex < 0) {
+            if (sourcex < 0) {
+                sourcex += pattern_offset + data->image->offsets[0];
                 row = pattern_row;
                 width = image->pattern->width;
             }
@@ -111,7 +124,7 @@ stereo_image_apply_lines_do(StereoImageApplyData *data, int start, int end,
                 width = image->image->width;
             }
 
-            blend(d, row, sourcex , width);
+            blend(d, row, sourcex, width);
             z += buffer->channels;
             d++;
         }
