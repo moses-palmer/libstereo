5f05afd13d7585a2f897737dea91b88beb557038
Moses Palmér <mosespalmer@gmail.com>
2011-01-03 21:47:42 +0000

    * Simplified code to make it actually work.

--- private/compile-glsl.sh
+++ private/compile-glsl.sh
@@ -9,31 +9,8 @@ name="`echo ${base##*/} | tr - _`"
 # Remove any previous version
 rm -f "$destination"
 
-documentation="`awk '
-BEGIN {
-    k = 0;
-}
-
-/\\s*\\*\\*\\// {
-    if (k == 1) k++;
-}
-
-/\\/\\*\\*/ {
-    k++;
-}
-
-{
-    if (k == 1) print;
-}
-' \"$path\"`"
-
-if [ ! -z "$documentation" ]; then
-    echo "$documentation"                                      >> "$destination"
-    echo                                                       >> "$destination"
-fi
-
 echo "#ifndef $name"                                           >> "$destination"
 echo "#define $name \\"                                        >> "$destination"
-cpp -P "$path" | sed -e 's/"/\\"/g;s/^/\"/g;s/$/\\n\" \\/g'    >> "$destination"
-echo "/* End of $path */"                                      >> "$destination"
+sed -e 's/.*/"&\\n" \\/g' "$path"                             >> "$destination"
+echo "\"\""                                                    >> "$destination"
 echo "#endif"                                                  >> "$destination"
