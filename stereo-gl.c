d3081a42c34d3a758acca86be62186f9440f3113
Moses Palmér <mosespalmer@gmail.com>
2011-01-09 15:44:24 +0000

    * Sanitised interface to OpenGL stereograms.

--- stereo-gl.c
+++ stereo-gl.c
@@ -1,26 +1,54 @@
+#include <malloc.h>
 #include <string.h>
 
 #include <GL/glew.h>
 
 #include "stereo-gl.h"
 
-#include <GL/glext.h>
-
 #include "private/stereo-shader.h"
 
-GLuint
-stereo_gl_program_create(void)
+struct StereoImageGL {
+    /* The stereogram program */
+    GLuint shader, program;
+
+    /* The background pattern to use */
+    StereoPattern *pattern;
+
+    /* The indice of the program uniform values */
+    struct {
+        GLint strength;
+        GLint left_margin;
+        GLint pattern;
+        GLint zbuffer;
+    } i;
+
+    /* The resourcfes used during runtime */
+    struct {
+        struct {
+            GLint pattern;
+        } textures;
+    } r;
+};
+
+StereoImageGL*
+stereo_image_gl_create(StereoPattern *pattern)
 {
+    StereoImageGL *result = NULL;
     const GLchar *stereo_shader_source = stereo_shader;
 
     GLuint shader, program;
     GLint length;
     GLint compiled, linked;
 
+    /* pattern cannot be NULL */
+    if (!pattern) {
+        return NULL;
+    }
+
     /* Create the shader */
     shader = glCreateShader(GL_FRAGMENT_SHADER);
     if (!shader) {
-        return 0;
+        return NULL;
     }
 
     /* Compile it... */
@@ -29,15 +57,19 @@ stereo_gl_program_create(void)
     glCompileShader(shader);
     glGetObjectParameterivARB(shader, GL_COMPILE_STATUS, &compiled);
     if (!compiled) {
+        GLchar buffer[1024];
+        glGetShaderInfoLog(shader, sizeof(buffer), NULL, buffer);
+        printf("Not compiled: %s.\n", buffer);
         glDeleteShader(shader);
-        return 0;
+        return NULL;
     }
 
     /* Create the program and attach the shader */
     program = glCreateProgram();
     if (!program) {
+        printf("No program.\n");
         glDeleteShader(shader);
-        return 0;
+        return NULL;
     }
     glAttachShader(program, shader);
 
@@ -45,9 +77,62 @@ stereo_gl_program_create(void)
     glLinkProgram(program);
     glGetObjectParameterivARB(program, GL_LINK_STATUS, &linked);
     if (!linked) {
+        printf("Not linked.\n");
+        glDeleteShader(shader);
         glDeleteProgram(program);
-        return 0;
+        return NULL;
     }
 
-    return program;
+    /* Create the result struct */
+    result = malloc(sizeof(*result));
+    memset(result, 0, sizeof(*result));
+    result->shader = shader;
+    result->program = program;
+    result->pattern = pattern;
+    result->i.strength = glGetUniformLocation(program, "strength");
+    result->i.left_margin  = glGetUniformLocation(program, "left_margin");
+    result->i.pattern  = glGetUniformLocation(program, "pattern");
+    result->i.zbuffer  = glGetUniformLocation(program, "zbuffer");
+
+    return result;
+}
+
+void
+stereo_image_gl_free(StereoImageGL *stereo_gl)
+{
+    glDeleteShader(stereo_gl->shader);
+    glDeleteProgram(stereo_gl->program);
+}
+
+int
+stereo_image_gl_start(StereoImageGL *stereo_gl, GLfloat strength,
+    GLuint zbuffer)
+{
+    glGenTextures(sizeof(stereo_gl->r.textures) / sizeof(GLuint),
+        (GLuint*)&stereo_gl->r.textures);
+
+    /* Create the pattern texture */
+    glBindTexture(GL_TEXTURE_2D, stereo_gl->r.textures.pattern);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+        stereo_gl->pattern->width, stereo_gl->pattern->height,
+        0, GL_RGBA, GL_UNSIGNED_BYTE, stereo_gl->pattern->pixels);
+
+    /* Set the program uniform values */
+    glUniform1f(stereo_gl->i.strength, strength);
+    glUniform1i(stereo_gl->i.left_margin, stereo_gl->pattern->width);
+    glUniform1i(stereo_gl->i.pattern, stereo_gl->r.textures.pattern);
+    glUniform1i(stereo_gl->i.zbuffer, zbuffer);
+
+    glUseProgram(stereo_gl->program);
+
+    return 0;
+}
+
+void
+stereo_image_gl_end(StereoImageGL *stereo_gl)
+{
+    glDeleteTextures(sizeof(stereo_gl->r.textures) / sizeof(GLuint),
+        (GLuint*)&stereo_gl->r.textures);
+    memset(&stereo_gl->r.textures, 0, sizeof(stereo_gl->r.textures));
 }
--- stereo-gl.h
+++ stereo-gl.h
@@ -3,6 +3,10 @@
 
 #include <GL/gl.h>
 
+#include "stereo.h"
+
+typedef struct StereoImageGL StereoImageGL;
+
 /**
  * Creates an OpenGL fragment shader program.
  *
@@ -12,9 +16,49 @@
  *   2. sampler2D pattern: The background pattern to use.
  *   3. sampler2DShadow zbuffer: The z-buffer that is rendered.
  *
- * @return a fragment shader program, or 0 if an error occurs.
+ * @param pattern
+ *     The background pattern to use.
+ * @return a stereogram that may be used as an OpenGL shader program.
  */
-GLuint
-stereo_gl_program_create(void);
+StereoImageGL*
+stereo_image_gl_create(StereoPattern *pattern);
+
+/**
+ * Frees a stereogram and all resources allocated by it.
+ *
+ * @param stereo_gl
+ *     The stereogram to free.
+ */
+void
+stereo_image_gl_free(StereoImageGL *stereo_gl);
+
+/**
+ * Sets the stereogram algorithm as the current fragments shader.
+ *
+ * stereo_image_gl_end must be called when this function
+ *
+ * @param stereo_gl
+ *     The stereogram.
+ * @param strength
+ *     The strength to use when applying the effect. The greater the strength,
+ *     the deeper the image appears.
+ * @param zbuffer
+ *     The z-buffer to use as source.
+ * @return 0 if the fragment shader was sucessfully set, otherise an error code
+ */
+int
+stereo_image_gl_start(StereoImageGL *stereo_gl, GLfloat strength,
+    GLuint zbuffer);
+
+/**
+ * Sets the stereogram algorithm as the current fragments shader.
+ *
+ * @param stereo_gl
+ *     The stereogram.
+ * @param zbuffer
+ *     The z-buffer to use as source.
+ */
+void
+stereo_image_gl_end(StereoImageGL *stereo_gl);
 
 #endif
